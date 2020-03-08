af49bb0bf69d9128ef86f4b218115635c2334e78
Jan Meppe <janmeppe@hotmail.com>
2020-03-08 15:04:50 +0100

    Add person with dynamic content

---
+++ doc/003-dynamic-content.md
@@ -0,0 +1,12 @@
+# Dynamic content
+
+```bash
+git checkout
+npm start
+```
+
+Should result in something like this
+
+## Key learnings
+
+* Use curly braces (`{..}`) to render dynamic content in JSX. 
--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Components/Person';
 
 class App extends Component {
+
     render() {
         return (
             <div className='App'>
                 <h1>Hi, I'm a React app</h1>
                 <p>This is working!</p>
+                <Person/>
+                <Person/>
+                <Person/>
             </div>
         );
     }
